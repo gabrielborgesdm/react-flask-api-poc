f1d65c54d51d70f68ef282b3065807cad8029967
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-05-19 16:11:42 -0300

    test: create base test class

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,3 +1,4 @@
+import unittest
 from flask import Flask
 
 from backend.app.handlers.http_error_handler import handle_exception
@@ -13,6 +14,29 @@ app.config["TESTING"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
+
 app.register_blueprint(books_bp)
 app.register_blueprint(authors_bp)
 app.register_error_handler(400, handle_exception)
+
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.db.session.close()
+        self.reset_db()
+
+    @classmethod
+    def reset_db(self):
+        self.db.drop_all()
+        self.db.create_all()
+
+    @classmethod
+    def setUpClass(self):
+        self.app = app
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.db = db
+
+    @classmethod
+    def tearDownClass(self):
+        self.app_context.pop()
