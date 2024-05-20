3d06a85a02d49df801ab6f1404e15e30af831e37
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-05-20 20:17:34 -0300

    feat: initialize Cors

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
 from backend.app.handlers.http_error_handler import handle_exception
@@ -13,12 +14,14 @@ from backend.app.controllers.author_controller import blueprint as authors_bp
 app = Flask(__name__)
 app.config.from_object(Config)
 
+cors = CORS(app)
+
 
 app.register_blueprint(books_bp)
-db.init_app(app)
 app.register_blueprint(authors_bp)
-
 app.register_error_handler(400, handle_exception)
+
+db.init_app(app)
 
 migrate = Migrate(app, db)
 ma = Marshmallow(app)
