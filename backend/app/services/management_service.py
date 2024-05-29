3502f6a7eb15c3c289a0d3784baaf294f3a5d323
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-05-29 15:12:10 -0300

    feat(backend): create method to deal with the db session management

--- backend/app/services/management_service.py
+++ backend/app/services/management_service.py
@@ -1,5 +1,6 @@
 from typing import Dict, List
 from werkzeug.exceptions import BadRequest
+from contextlib import contextmanager
 
 from backend.app.models.model import db
 from backend.app.models.author_model import AuthorModel
@@ -9,30 +10,57 @@ from backend.app.schemas.book_schema import AuthorFindOrCreateSchema, books_sche
 
 
 class ManagementService:
+    @contextmanager
+    def session_scope(self):
+        try:
+            db.session.begin()
+            yield
+        except Exception as e:
+            db.session.rollback()
+            print(f"An error occurred: {e}")
+            raise e
+        finally:
+            db.session.close()
+
     def get_authors(self):
-        authors = db.session.query(AuthorModel).all()
-        return authors_schema.dump(authors)
+        authors_response = []
+        with self.session_scope():
+            authors = db.session.query(AuthorModel).all()
+            authors_response = authors_schema.dump(authors)
+        return authors_response
 
     def get_books(self):
-        books = db.session.query(BookModel).all()
-        return books_schema.dump(books)
+        books_response = []
+        with self.session_scope():
+            books = db.session.query(BookModel).all()
+            books_response = books_schema.dump(books)
+
+        return books_response
 
     def create_author(self, author_data: Dict) -> AuthorSchema:
-        with db.session.begin(True):
-            author = AuthorModel(**author_data)
-            db.session.add(author)
-        return author_schema.dump(author)
+        author_response = None
+
+        with self.session_scope():
+            author_model = AuthorModel(**author_data)
+            db.session.add(author_model)
+            db.session.commit()
+            author_response = author_schema.dump(author_model)
+
+        return author_response
 
     def create_book(self, book_dto: Dict):
-        with db.session.begin(True):
+        book_response = None
+        with self.session_scope():
             authors = self._get_authors_for_book_creation(book_dto.get("authors"))
-
             del book_dto["authors"]
+
             book = BookModel(**book_dto)
             book.authors = authors
             db.session.add(book)
+            db.session.commit()
+            book_response = book_schema.dump(book)
 
-        return book_schema.dump(book)
+        return book_response
 
     def _get_authors_for_book_creation(self, authors_dto: List[AuthorFindOrCreateSchema]):
         authors = [self._find_or_create_author(author) for author in authors_dto]
