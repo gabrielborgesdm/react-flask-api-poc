91397046e3dabafbc6f8e4c84b97e8939870e8cd
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-06-03 23:41:31 -0300

    feat(frontend): implement reusable filter component

--- frontend/src/components/Hooks/UseFilterHook.tsx
+++ frontend/src/components/Hooks/UseFilterHook.tsx
@@ -1,33 +1,33 @@
 import { ChangeEvent, useState } from "react";
 
 export const useFilterHook = () => {
-    const [filter, setFilter] = useState<string | null>(null)
+  const [filter, setFilter] = useState<string | null>(null);
 
-    const handleChangeFilter = (e: ChangeEvent<HTMLInputElement>): void => {
-        setFilter(e.target.value)
+  const handleChangeFilter = (e: ChangeEvent<HTMLInputElement>): void => {
+    setFilter(e.target.value);
+  };
+
+  const shouldFilterInWith = (...properties: (unknown | undefined)[]): boolean => {
+    if (!filter) return true;
+
+    let anyPropertyMatch = false;
+
+    for (const property of properties) {
+      if (typeof property === "undefined") {
+        continue;
+      }
+
+      if (String(property).toLowerCase().includes(filter.toLowerCase())) {
+        anyPropertyMatch = true;
+        break;
+      }
     }
 
-    const shouldFilterInWith = (...properties: (unknown | undefined)[]): boolean => {
-        if (!filter) return true
+    return anyPropertyMatch;
+  };
 
-        let anyPropertyMatch = false
-
-        for (const property of properties) {
-            if (typeof property === "undefined") {
-                continue
-            }
-
-            if (String(property).toLowerCase().includes(filter.toLowerCase())) {
-                anyPropertyMatch = true
-                break
-            }
-        }
-
-        return anyPropertyMatch
-    }
-
-    return {
-        handleChangeFilter,
-        shouldFilterInWith,
-    }
-}
+  return {
+    handleChangeFilter,
+    shouldFilterInWith,
+  };
+};
