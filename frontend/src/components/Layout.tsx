b50335da39f7ec01b2dec57940bc4a73e681da2f
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-06-03 23:43:21 -0300

    feat(frontend): implement resources listing pages

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -2,13 +2,12 @@ import { Outlet } from "react-router-dom";
 import { Navbar } from "./Navbar";
 
 const Layout: React.FC = () => (
-    <>
-        <Navbar />
-        <div className="sm:rounded-lg w-1/2 mx-auto mt-4">
-            <Outlet />
-        </div>
-    </>
+  <>
+    <Navbar />
+    <div className="sm:rounded-lg w-1/2 mx-auto mt-4">
+      <Outlet />
+    </div>
+  </>
 );
 
-export default Layout
-
+export default Layout;
--- frontend/src/pages/Authors/Authors.tsx
+++ frontend/src/pages/Authors/Authors.tsx
@@ -1,87 +1,102 @@
-import React, { useEffect, useState } from 'react';
-import ManagementService from 'services/managementService';
-import { useFilterHook } from 'components/Hooks/UseFilterHook';
-import { Author } from 'types/author';
+import React, { useEffect, useState } from "react";
+import ManagementService from "services/managementService";
+import { useFilterHook } from "components/Hooks/UseFilterHook";
+import { Author } from "types/author";
 
-const managementService = new ManagementService()
-
+const managementService = new ManagementService();
 
 const Authors: React.FC = () => {
-    const [authors, setAuthors] = useState<Author[]>([])
-    const { handleChangeFilter, shouldFilterInWith } = useFilterHook();
+  const [authors, setAuthors] = useState<Author[]>([]);
+  const { handleChangeFilter, shouldFilterInWith } = useFilterHook();
 
-    useEffect(() => {
-        loadAuthors()
-    }, [])
+  useEffect(() => {
+    loadAuthors();
+  }, []);
 
-    const loadAuthors = async () => {
-        const authors = await managementService.getAuthors()
+  const loadAuthors = async () => {
+    const authors = await managementService.getAuthors();
 
-        setAuthors(authors)
-    }
+    setAuthors(authors);
+  };
 
-
-    return (
-        <>
-            <div className="flex items-center py-2 justify-between">
-                <label htmlFor="table-search" className="sr-only">Search</label>
-                <div className="relative">
-                    <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
-                        <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
-                        </svg>
-                    </div>
-                    <input type="text" id="table-search" onChange={handleChangeFilter} className="block py-3 ps-10 text-sm border rounded-lg w-80 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search for items" />
-                </div>
-                <a href='/authors/create' className="max-sm:w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    + Add author
-                </a>
-            </div>
-            <hr />
-            <div className='block max-h-[78dvh] overflow-y-auto'>
-                <table className="w-full text-sm text-left rtl:text-right text-gray-500">
-                    <thead className="text-xs uppercase ">
-                        <tr>
-
-                            <th scope="col" className="px-1 py-3">
-                                Author
-                            </th>
-                            <th scope="col" className="px-1 py-3">
-                                Nationality
-                            </th>
-                            <th scope="col" className="px-1 py-3">
-                                Birth date
-                            </th>
-                            <th scope="col" className="px-1 py-3">
-                                E-mail
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {authors?.length > 0 && authors.map(({ name, nationality, birthDate, email, id }: Author) => (
-                            shouldFilterInWith(name, nationality, birthDate, email, id)
-                            && (
-                                <tr key={id} className="border-b hover:bg-gray-50">
-                                    <th scope="row" className="px-1 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                        {name}
-                                    </th>
-                                    <td className="px-1 py-4">
-                                        {nationality}
-                                    </td>
-                                    <td className="px-1 py-4">
-                                        {birthDate}
-                                    </td>
-                                    <td className="px-1 py-4">
-                                        {email}
-                                    </td>
-                                </tr>
-                            )
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex items-center py-2 justify-between">
+        <label htmlFor="table-search" className="sr-only">
+          Search
+        </label>
+        <div className="relative">
+          <div className="absolute inset-y-0 rtl:inset-r-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="text"
+            id="table-search"
+            onChange={handleChangeFilter}
+            className="block py-3 ps-10 text-sm border rounded-lg w-80 focus:ring-blue-500 focus:border-blue-500 "
+            placeholder="Search for items"
+          />
+        </div>
+        <a
+          href="/authors/create"
+          className="max-sm:w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        >
+          + Add author
+        </a>
+      </div>
+      <hr />
+      <div className="block max-h-[78dvh] overflow-y-auto">
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+          <thead className="text-xs uppercase ">
+            <tr>
+              <th scope="col" className="px-1 py-3">
+                Author
+              </th>
+              <th scope="col" className="px-1 py-3">
+                Nationality
+              </th>
+              <th scope="col" className="px-1 py-3">
+                Birth date
+              </th>
+              <th scope="col" className="px-1 py-3">
+                E-mail
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {authors?.length > 0 &&
+              authors.map(
+                ({ name, nationality, birthDate, email, id }: Author) =>
+                  shouldFilterInWith(name, nationality, birthDate, email, id) && (
+                    <tr key={id} className="border-b hover:bg-gray-50">
+                      <th scope="row" className="px-1 py-4 font-medium text-gray-900 whitespace-nowrap">
+                        {name}
+                      </th>
+                      <td className="px-1 py-4">{nationality}</td>
+                      <td className="px-1 py-4">{birthDate}</td>
+                      <td className="px-1 py-4">{email}</td>
+                    </tr>
+                  ),
+              )}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
 };
 
 export default Authors;
