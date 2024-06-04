be7c722ec7970c45c550be1d9cfed2598bb998a9
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2024-06-03 23:55:18 -0300

    fix(frontend): fix form type

--- frontend/src/components/AuthorForm.tsx
+++ frontend/src/components/AuthorForm.tsx
@@ -1,84 +1,63 @@
 import React, { useState } from "react";
-import { AuthorCreate } from "types/author";
 import Input from "./Form/Input";
 import { SubmitHandler, useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup"
+import { yupResolver } from "@hookform/resolvers/yup";
 import AuthorSchema from "./Schemas/AuthorSchema";
 import Form, { FormMessage } from "./Form/Form";
 import ManagementService from "services/managementService";
+import { AuthorFormCreate } from "types/author";
 
-const managementService = new ManagementService()
-
+const managementService = new ManagementService();
 
 const AuthorForm: React.FC = () => {
-    const messageInitialState = { value: "", isError: false }
-    const [isLoading, setIsLoading] = useState(false)
-    const [message, setMessage] = useState<FormMessage>(messageInitialState)
-    const {
-        register,
-        handleSubmit,
-        reset,
+  const messageInitialState = { value: "", isError: false };
+  const [isLoading, setIsLoading] = useState(false);
+  const [message, setMessage] = useState<FormMessage>(messageInitialState);
+  const {
+    register,
+    handleSubmit,
+    reset,
 
-        formState: { errors },
-    } = useForm({ resolver: yupResolver(AuthorSchema) })
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(AuthorSchema) });
 
+  const onSubmit: SubmitHandler<AuthorFormCreate> = async (data: AuthorFormCreate) => {
+    setIsLoading(true);
+    const author = await managementService.createAuthor(data);
+    console.log(author, author === undefined);
+    setMessage({
+      value: author ? "Author created with success" : "It wansn't possible to create the author, try again",
+      isError: author === undefined,
+    });
+    setIsLoading(false);
+    // onReset()
+  };
 
+  const onReset = () => {
+    setMessage(messageInitialState);
+    reset(undefined);
+  };
 
-    const onSubmit: SubmitHandler<AuthorCreate> = async (data: AuthorCreate) => {
-        setIsLoading(true)
-        const author = await managementService.createAuthor(data)
-        console.log(author, author === undefined)
-        setMessage({
-            value: author ? "Author created with success" : "It wansn't possible to create the author, try again",
-            isError: author === undefined
-        })
-        setIsLoading(false)
-        // onReset()
-    }
+  return (
+    <Form
+      onSubmit={handleSubmit(onSubmit)}
+      onReset={onReset}
+      title="Author Creation"
+      description="Enter the author details"
+      message={message}
+      isLoading={isLoading}
+    >
+      <>
+        <Input label="Author name" name="name" register={register} errorMessage={errors.name?.message} />
 
-    const onReset = () => {
-        setMessage(messageInitialState)
-        reset(undefined)
-    }
+        <Input label="Email address" name="email" register={register} errorMessage={errors.email?.message} />
 
-    return (
-        <Form
-            onSubmit={handleSubmit(onSubmit)}
-            onReset={onReset}
-            title="Author Creation"
-            description="Enter the author details"
-            message={message}
-            isLoading={isLoading}>
-            <>
-                <Input
-                    label="Author name"
-                    name="name"
-                    register={register}
-                    errorMessage={errors.name?.message}
-                />
+        <Input label="Nationality" name="nationality" register={register} errorMessage={errors.nationality?.message} />
 
-                <Input
-                    label="Email address"
-                    name="email"
-                    register={register}
-                    errorMessage={errors.email?.message}
-                />
+        <Input label="Birth date" name="birthDate" register={register} errorMessage={errors.birthDate?.message} />
+      </>
+    </Form>
+  );
+};
 
-                <Input
-                    label="Nationality"
-                    name="nationality"
-                    register={register}
-                    errorMessage={errors.nationality?.message}
-                />
-
-                <Input
-                    label="Birth date"
-                    name="birthDate"
-                    register={register}
-                    errorMessage={errors.birthDate?.message}
-                /></>
-        </Form>
-    )
-}
-
-export default AuthorForm
+export default AuthorForm;
